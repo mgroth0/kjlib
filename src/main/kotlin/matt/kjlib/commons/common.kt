e1c5d97f424c89500acef299ad338d43b61ba91c
Matt Groth <mgroth49@gmail.com>
2021-05-08 18:05:00 -0400

    idk

--- src/main/kotlin/matt/kjlib/commons/common.kt
+++ src/main/kotlin/matt/kjlib/commons/common.kt
@@ -3,7 +3,7 @@ package matt.kjlib.commons
 import matt.kjlib.get
 import java.io.File
 
-val REGISTERED_FOLDER = File(File(System.getProperty("user.dir")).resolve(".registeredDir.txt").readText())
+val REGISTERED_FOLDER = File(File(System.getProperty("user.home")).resolve(".registeredDir.txt").readText())
 val ROOT_FOLDER = REGISTERED_FOLDER["todo/flow"]
 val DNN_FOLDER = REGISTERED_FOLDER["todo/science/dnn"]
 val DATA_FOLDER = ROOT_FOLDER["data"]
