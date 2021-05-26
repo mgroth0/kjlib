e946a9dadda305c1ce8153fc4e4b34a0643949e4
Matt Groth <mgroth49@gmail.com>
2021-05-26 18:58:37 -0400

    autocommit

--- src/main/kotlin/matt/kjlib/commons/common.kt
+++ src/main/kotlin/matt/kjlib/commons/common.kt
@@ -6,6 +6,8 @@ import java.io.File
 
 val REGISTERED_FOLDER = File(File(System.getProperty("user.home")).resolve(".registeredDir.txt").readText().trim())
 val ROOT_FOLDER = resourceTxt("matt/rootFolder.txt")?.let { File(it) } ?: REGISTERED_FOLDER["todo/flow"]
+val USER_DIR = File(System.getProperty("user.dir"))
+val HOME_DIR = File(System.getProperty("user.home"))
 val DNN_FOLDER = REGISTERED_FOLDER["todo/science/dnn"]
 val DATA_FOLDER = ROOT_FOLDER["data"]
 val WINDOW_GEOMETRY_FOLDER = DATA_FOLDER["window"]
