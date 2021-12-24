83fe0e632e27d5db511e7f873c34aeca98e0b2f8
mgroth0 <mgroth49@gmail.com>
2021-12-24 18:53:51 -0500

    update submod

--- src/main/kotlin/matt/kjlib/date/date.kt
+++ src/main/kotlin/matt/kjlib/date/date.kt
@@ -22,63 +22,65 @@ import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind.EXACTLY_ONCE
 import kotlin.contracts.contract
 
+var simplePrinting = false
+
 class Duration private constructor(nanos: Long) {
 
-  constructor(startNanos: Number, stopNanos: Number): this(
-	(stopNanos.toDouble() - startNanos.toDouble()).toLong()
-  )
+    constructor(startNanos: Number, stopNanos: Number) : this(
+            (stopNanos.toDouble() - startNanos.toDouble()).toLong()
+    )
 
-  private val stupidDur = java.time.Duration.ofNanos(nanos)
+    private val stupidDur = java.time.Duration.ofNanos(nanos)
 
-  companion object {
-	val purpose =
-		"because stupid kotlin duration is \"experimental\", requiring weird annotations and not working half the time, while stupid java Duration is even more stupid because all the methods take and give whole numbers rather than Doubles. The nice thing is if I eventually want to switch to kotlin.time.Duration backend, it wont be a huge task"
+    companion object {
+        val purpose =
+                "because stupid kotlin duration is \"experimental\", requiring weird annotations and not working half the time, while stupid java Duration is even more stupid because all the methods take and give whole numbers rather than Doubles. The nice thing is if I eventually want to switch to kotlin.time.Duration backend, it wont be a huge task"
 
-	fun ofDays(days: Number) = Duration((days.toDouble()*60*60*24*BILLION).toLong())
-	fun ofHours(hours: Number) = Duration((hours.toDouble()*60*60*BILLION).toLong())
-	fun ofMinutes(min: Number) = Duration((min.toDouble()*60*BILLION).toLong())
-	fun ofSeconds(sec: Number) = Duration((sec.toDouble()*BILLION).toLong())
-	fun ofMilliseconds(ms: Number) = Duration((ms.toDouble()*MILLION).toLong())
-	fun ofNanoseconds(nanos: Number) = Duration(nanos.toLong())
-  }
+        fun ofDays(days: Number) = Duration((days.toDouble() * 60 * 60 * 24 * BILLION).toLong())
+        fun ofHours(hours: Number) = Duration((hours.toDouble() * 60 * 60 * BILLION).toLong())
+        fun ofMinutes(min: Number) = Duration((min.toDouble() * 60 * BILLION).toLong())
+        fun ofSeconds(sec: Number) = Duration((sec.toDouble() * BILLION).toLong())
+        fun ofMilliseconds(ms: Number) = Duration((ms.toDouble() * MILLION).toLong())
+        fun ofNanoseconds(nanos: Number) = Duration(nanos.toLong())
+    }
 
 
-  val inMinutes by lazy {
-	THOUSAND
-	stupidDur.toNanos().toDouble()/MILLION/THOUSAND/60.0
-  }
-  val inSeconds by lazy {
-	stupidDur.toNanos().toDouble()/MILLION/THOUSAND
-  }
-  val inMilliseconds by lazy {
-	stupidDur.toNanos().toDouble()/MILLION
-  }
-  val inMicroseconds by lazy {
-	stupidDur.toNanos().toDouble()/THOUSAND
-  }
-  val inNanoseconds by lazy {
-	stupidDur.toNanos().toDouble()
-  }
+    val inMinutes by lazy {
+        THOUSAND
+        stupidDur.toNanos().toDouble() / MILLION / THOUSAND / 60.0
+    }
+    val inSeconds by lazy {
+        stupidDur.toNanos().toDouble() / MILLION / THOUSAND
+    }
+    val inMilliseconds by lazy {
+        stupidDur.toNanos().toDouble() / MILLION
+    }
+    val inMicroseconds by lazy {
+        stupidDur.toNanos().toDouble() / THOUSAND
+    }
+    val inNanoseconds by lazy {
+        stupidDur.toNanos().toDouble()
+    }
 
-  fun format(): String {
-	return when {
-	  inMinutes >= 2.0      -> "${inMinutes.roundToDecimal(2)} min"
-	  inSeconds >= 2.0      -> "${inSeconds.roundToDecimal(2)} sec"
-	  inMilliseconds >= 2.0 -> "${inMilliseconds.roundToDecimal(2)} ms"
-	  inMicroseconds >= 2.0 -> "${inMicroseconds.roundToDecimal(2)} μs"
-	  else                  -> "${inNanoseconds.roundToDecimal(2)} ns"
+    fun format(): String {
+        return when {
+            inMinutes >= 2.0 -> "${inMinutes.roundToDecimal(2)} min"
+            inSeconds >= 2.0 -> "${inSeconds.roundToDecimal(2)} sec"
+            inMilliseconds >= 2.0 -> "${inMilliseconds.roundToDecimal(2)} ms"
+            inMicroseconds >= 2.0 -> "${inMicroseconds.roundToDecimal(2)} μs"
+            else -> "${inNanoseconds.roundToDecimal(2)} ns"
 
-	}
-  }
+        }
+    }
 
-  override fun toString() = format()
+    override fun toString() = format()
 
 }
 
 val Number.unixSeconds: Date
-  get() = Date((this.toDouble()*1000).toLong())
+    get() = Date((this.toDouble() * 1000).toLong())
 val Number.unixMS: Date
-  get() = Date(this.toLong())
+    get() = Date(this.toLong())
 
 
 private val stupid = "Have to keep it as a different name than Duration.format since they are in the same package???"
@@ -96,140 +98,142 @@ fun milli() = System.currentTimeMillis()
 
 fun LocalDateTime.atTime(hour: Int, min: Int) = toLocalDate().atTime(hour, min)
 private val OFFSET = OffsetDateTime.now().offset
-fun LocalDateTime.toEpochMilli() = toEpochSecond(OFFSET)*1000
+fun LocalDateTime.toEpochMilli() = toEpochSecond(OFFSET) * 1000
 
 operator fun Date.minus(started: Date): Duration {
-  return this.toInstant() - started.toInstant()
+    return this.toInstant() - started.toInstant()
 }
 
 operator fun Instant.minus(started: Instant): Duration {
-  return (this.epochSecond - started.epochSecond).sec
+    return (this.epochSecond - started.epochSecond).sec
 }
 
 operator fun Date.minus(started: Instant): Duration {
-  return toInstant() - started
+    return toInstant() - started
 }
 
 operator fun Instant.minus(started: Date): Duration {
-  return this - started.toInstant()
+    return this - started.toInstant()
 }
 
 fun println_withtime(s: String) {
-  println(System.currentTimeMillis().toString() + ":" + s)
+    println(System.currentTimeMillis().toString() + ":" + s)
 }
 
 val Number.nanos
-  get() = Duration.ofNanoseconds(this)
+    get() = Duration.ofNanoseconds(this)
 val Number.ms
-  get() = Duration.ofMilliseconds(this)
+    get() = Duration.ofMilliseconds(this)
 val Number.sec
-  get() = Duration.ofSeconds(this)
+    get() = Duration.ofSeconds(this)
 val Number.min
-  get() = Duration.ofMinutes(this)
+    get() = Duration.ofMinutes(this)
 val Number.hours
-  get() = Duration.ofHours(this)
+    get() = Duration.ofHours(this)
 val Number.days
-  get() = Duration.ofDays(this)
+    get() = Duration.ofDays(this)
 
 fun now() = System.currentTimeMillis().unixMS
 
 
 @ExperimentalContracts
-fun <R> stopwatch(s: String, op: ()->R): R {
-  contract {
-	callsInPlace(op, EXACTLY_ONCE)
-  }
-  println("timing ${s}...")
-  val start = System.nanoTime()
-  val r = op()
-  val stop = System.nanoTime()
-  val dur = Duration(start, stop)
-  println("$s took $dur")
-  return r
+fun <R> stopwatch(s: String, op: () -> R): R {
+    contract {
+        callsInPlace(op, EXACTLY_ONCE)
+    }
+    println("timing ${s}...")
+    val start = System.nanoTime()
+    val r = op()
+    val stop = System.nanoTime()
+    val dur = Duration(start, stop)
+    println("$s took $dur")
+    return r
 }
 
 data class Stopwatch(
-  val startRelativeNanos: Long,
-  val enabled: Boolean = true,
-  val printWriter: PrintWriter? = null,
-  val prefix: String? = null
+        val startRelativeNanos: Long,
+        val enabled: Boolean = true,
+        val printWriter: PrintWriter? = null,
+        val prefix: String? = null
 ) {
-  val prefixS = if (prefix != null) "$prefix\t" else ""
-  fun toc(s: String) {
-	if (enabled) {
-	  val stop = System.nanoTime()
-	  val dur = Duration(startRelativeNanos, stop)
-	  if (printWriter == null) {
-		println("$dur\t$prefixS$s")
-	  } else {
-		printWriter.println("$dur\t$prefixS$s")
-	  }
+    val prefixS = if (prefix != null) "$prefix\t" else ""
+    fun toc(s: String) {
+        if (enabled) {
+            val stop = System.nanoTime()
+            val dur = Duration(startRelativeNanos, stop)
+            if (simplePrinting) {
+                println("$dur\t$s")
+            } else if (printWriter == null) {
+                println("$dur\t$prefixS$s")
+            } else {
+                printWriter.println("$dur\t$prefixS$s")
+            }
 
-	}
-  }
+        }
+    }
 }
 
 private val ticSem = Semaphore(1)
 val keysForNestedStuffUsedRecently = mutableMapOf<String, Int>().apply {
-  every(2.sec, ownTimer = true) {
-	ticSem.with {
-	  clear()
-	}
-  }
+    every(2.sec, ownTimer = true) {
+        ticSem.with {
+            clear()
+        }
+    }
 }
 
 fun tic(
-  enabled: Boolean = true,
-  printWriter: PrintWriter? = null,
-  keyForNestedStuff: String? = null,
-  nestLevel: Int = 1,
-  prefix: String? = null
+        enabled: Boolean = true,
+        printWriter: PrintWriter? = null,
+        keyForNestedStuff: String? = null,
+        nestLevel: Int = 1,
+        prefix: String? = null
 ): Stopwatch {
-  var realEnabled = enabled
-  if (enabled) {
-	ticSem.with {
-	  if (keyForNestedStuff in keysForNestedStuffUsedRecently && nestLevel == keysForNestedStuffUsedRecently[keyForNestedStuff]) {
-		realEnabled = false
-	  } else if (keyForNestedStuff != null) {
-		if (keyForNestedStuff in keysForNestedStuffUsedRecently) {
-		  keysForNestedStuffUsedRecently[keyForNestedStuff] = keysForNestedStuffUsedRecently[keyForNestedStuff]!! + 1
-		} else {
-		  keysForNestedStuffUsedRecently[keyForNestedStuff] = 1
-		}
-	  }
-	}
-  }
-  val start = System.nanoTime()
-  val sw = Stopwatch(start, enabled = realEnabled, printWriter = printWriter, prefix = prefix)
-  if (realEnabled) {
-	println() /*to visually space this stopwatch print statements*/
-  }
+    var realEnabled = enabled
+    if (enabled) {
+        ticSem.with {
+            if (keyForNestedStuff in keysForNestedStuffUsedRecently && nestLevel == keysForNestedStuffUsedRecently[keyForNestedStuff]) {
+                realEnabled = false
+            } else if (keyForNestedStuff != null) {
+                if (keyForNestedStuff in keysForNestedStuffUsedRecently) {
+                    keysForNestedStuffUsedRecently[keyForNestedStuff] = keysForNestedStuffUsedRecently[keyForNestedStuff]!! + 1
+                } else {
+                    keysForNestedStuffUsedRecently[keyForNestedStuff] = 1
+                }
+            }
+        }
+    }
+    val start = System.nanoTime()
+    val sw = Stopwatch(start, enabled = realEnabled, printWriter = printWriter, prefix = prefix)
+    if (realEnabled && !simplePrinting) {
+        println() /*to visually space this stopwatch print statements*/
+    }
 
 
 
-  return sw
+    return sw
 }
 
 private var globalsw: Stopwatch? = null
 fun globaltic(enabled: Boolean = true) {
-  globalsw = tic(enabled = enabled)
+    globalsw = tic(enabled = enabled)
 }
 
 fun globaltoc(s: String) {
-  if (globalsw == null) {
-	println("gotta use globaltic first:${s}")
-  } else {
-	globalsw!!.toc(s)
-  }
+    if (globalsw == null) {
+        println("gotta use globaltic first:${s}")
+    } else {
+        globalsw!!.toc(s)
+    }
 }
 
-inline fun <R> withStopwatch(s: String, op: (Stopwatch)->R): R {
-  contract {
-	callsInPlace(op, EXACTLY_ONCE)
-  }
-  val t = tic()
-  t.toc("starting stopwatch: $s")
-  val r = op(t)
-  t.toc("finished stopwatch: $s")
-  return r
+inline fun <R> withStopwatch(s: String, op: (Stopwatch) -> R): R {
+    contract {
+        callsInPlace(op, EXACTLY_ONCE)
+    }
+    val t = tic()
+    t.toc("starting stopwatch: $s")
+    val r = op(t)
+    t.toc("finished stopwatch: $s")
+    return r
 }
