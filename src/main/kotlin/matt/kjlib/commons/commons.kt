4a8b4f6f1dc428efdb4ca7146f8e4e3117682cdf
mgroth0 <mgroth49@gmail.com>
2022-02-20 08:51:53 -0500

    autocommit

--- src/main/kotlin/matt/kjlib/commons/commons.kt
+++ src/main/kotlin/matt/kjlib/commons/commons.kt
@@ -7,13 +7,13 @@ import java.io.File
 
 val USER_HOME = File(System.getProperty("user.home"))
 val REGISTERED_FOLDER = File(USER_HOME.resolve(".registeredDir.txt").readText().trim())
-val ROOT_FOLDER = USER_HOME.resolve(resourceTxt("rootFolder.txt")?.let { File(it) }
+val FLOW_FOLDER = USER_HOME.resolve(resourceTxt("rootFolder.txt")?.let { File(it) }
   ?: (if (isNewMac) REGISTERED_FOLDER["flow"] else REGISTERED_FOLDER["todo/flow"]))
-val LOG_FOLDER = ROOT_FOLDER.resolve("log").apply { mkdir() }
+val LOG_FOLDER = FLOW_FOLDER.resolve("log").apply { mkdir() }
 val USER_DIR = File(System.getProperty("user.dir"))
 val TEMP_DIR = USER_DIR["tmp"].apply { mkdir() }
 val DNN_FOLDER = if (isNewMac) REGISTERED_FOLDER["ide/dnn"] else REGISTERED_FOLDER["todo/science/dnn"]
-val DATA_FOLDER = ROOT_FOLDER["data"]
+val DATA_FOLDER = FLOW_FOLDER["data"]
 val WINDOW_GEOMETRY_FOLDER = DATA_FOLDER["window"]
 val SETTINGS_FOLDER = DATA_FOLDER["settings"]
 
