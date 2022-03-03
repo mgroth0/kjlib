2a13d98f55a5cfb1049613fd56c45c423b14cb13
mgroth0 <mgroth49@gmail.com>
2022-03-02 20:35:15 -0500

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 dependencies {
-  api(projects.kj.klibexport)
-  api(projects.kj.reflect)
-  api(libs.bundles.multik.full)
-  implementation(libs.apfloat)
-  implementation(libs.aparapi)
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-  /*api("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
+    api(projects.kj.klibexport)
+    api(projects.kj.reflect)
+    api(libs.bundles.multik.full)
+    implementation(libs.apfloat)
+    implementation(libs.aparapi)
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    /*api("org.tensorflow:tensorflow-core-api:0.4.0")*/
+    /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
+    /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
 }
 /*
 kotlin {
