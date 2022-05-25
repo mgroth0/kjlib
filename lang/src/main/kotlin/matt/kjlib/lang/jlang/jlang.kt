19bc101c8de1cc4d93d15d0f7e2795f0965cf168
mgroth0 <mgroth49@gmail.com>
2022-05-25 02:31:35 -0400

    autocommit

--- lang/src/main/kotlin/matt/kjlib/lang/jlang/jlang.kt
+++ lang/src/main/kotlin/matt/kjlib/lang/jlang/jlang.kt
@@ -5,3 +5,6 @@ val runtime = Runtime.getRuntime()!!
 /*Thing()::class.java.classLoader*/
 /*ClassLoader.getPlatformClassLoader()*/
 fun resourceTxt(name: String) = ClassLoader.getSystemClassLoader().getResourceAsStream(name)?.bufferedReader()?.readText()
+
+val os: String by lazy { System.getProperty("os.name") }
+val ismac by lazy { os.startsWith("Mac") }
