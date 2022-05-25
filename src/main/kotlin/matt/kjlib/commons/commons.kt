63559ecc17613725caade4d18a6d3aea1028da48
mgroth0 <mgroth49@gmail.com>
2022-05-25 13:33:44 -0400

    autocommit

--- src/main/kotlin/matt/kjlib/commons/commons.kt
+++ src/main/kotlin/matt/kjlib/commons/commons.kt
@@ -13,7 +13,7 @@ val USER_DIR = File(System.getProperty("user.dir"))
 val TEMP_DIR = USER_DIR["tmp"].apply { mkdir() }
 val DNN_FOLDER = if (isNewMac) REGISTERED_FOLDER["ide/dnn"] else REGISTERED_FOLDER["todo/science/dnn"]
 
-//val REGISTERED_DATA_FOLDER = REGISTERED_FOLDER["data"]
+//val REGISTERED_DATA_FOLDER = matt.klib.getREGISTERED_FOLDER["data"]
 val WINDOW_GEOMETRY_FOLDER = DATA_FOLDER["window"]
 val SETTINGS_FOLDER = DATA_FOLDER["settings"]
 val VAR_JSON = DATA_FOLDER["VAR.json"]
