ace2f364bde8ed7ea93efb4604fb08a2bccf74a8
mgroth0 <mgroth49@gmail.com>
2022-05-30 05:55:08 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,16 @@
 dependencies {
-  //  api(projects.kj.klibexport)
   api(jvm(projects.k.klib))
   api(projects.kj.reflect)
-  //  api(projects.kj.kbuild)
   api(projects.kj.kjlib.lang)
   api(jvm(projects.k.stream))
   api(projects.kj.kjlib.jmath)
   implementation(libs.aparapi)
-
-  //  api(projects.kj.temp)
-
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-
-
-  //  api(libs.oshi)
-
-
   api(libs.kotlinx.serialization.json)
-
-
-  /*api("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
 }
 
 
 
 plugins {
   kotlin("plugin.serialization")
-
-  /*experimental.coroutines = org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE*/
 }
 
-/*
-configurations.all {
-  resolutionStrategy.dependencySubstitution {
-	substitute(module("org.tensorflow:tensorflow-core-api"))
-	  .using(module("org.tensorflow:tensorflow-core-api:0.4.0"))
-	  .withClassifier("macosx-x86_64")
-  }
-}*/
