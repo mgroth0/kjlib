337037c359b20d8af2c083455d72554a4d9229be
mgroth0 <mgroth49@gmail.com>
2022-06-05 04:29:46 -0400

    autocommit

--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -4,23 +4,6 @@ modtype = LIB
 
 
 dependencies {
-//  projects.k.klib
   projectOrLocalMavenJVM("api", ":k:klib")
-//  if (findProject(":k:klib") != null) {
-//	api(project(":k:klib")) {
-//	  targetConfiguration = "jvmRuntimeElements"
-//	}
-//  } else {
-//	api("matt.k:klib:+")
-//  }
-  //  if (rootDir.name.upper() == "FLOW") {
-  //	api(project(":k:klib")) {
-  //	  targetConfiguration = "jvmRuntimeElements"
-  //	}
-  //  } else {
-  //	api("matt.k:klib:+")
-  //  }
   kotlin("reflect")
-
-
 }
