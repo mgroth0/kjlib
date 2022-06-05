89989adff41cbd76072618ab5c9ef2e7b9378514
mgroth0 <mgroth49@gmail.com>
2022-06-05 04:26:04 -0400

    autocommit

--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -1,22 +1,25 @@
+import matt.kbuild.gbuild.projectOrLocalMavenJVM
+
 modtype = LIB
 
 
 dependencies {
-  projects.k.klib
-  if (findProject(":k:klib") != null) {
-	api(project(":k:klib")) {
-	  targetConfiguration = "jvmRuntimeElements"
-	}
-  } else {
-	api("matt.k:klib:+")
-  }
-//  if (rootDir.name.upper() == "FLOW") {
+//  projects.k.klib
+  projectOrLocalMavenJVM("api", ":k:klib")
+//  if (findProject(":k:klib") != null) {
 //	api(project(":k:klib")) {
 //	  targetConfiguration = "jvmRuntimeElements"
 //	}
 //  } else {
 //	api("matt.k:klib:+")
 //  }
+  //  if (rootDir.name.upper() == "FLOW") {
+  //	api(project(":k:klib")) {
+  //	  targetConfiguration = "jvmRuntimeElements"
+  //	}
+  //  } else {
+  //	api("matt.k:klib:+")
+  //  }
   kotlin("reflect")
 
 
