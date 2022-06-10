cc1bf93b488f1562c91da37a5217fc94072bbd1b
mgroth0 <mgroth49@gmail.com>
2022-06-09 22:58:03 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,8 @@
 import matt.klib.str.upper
 modtype = LIB
 dependencies {
-  if (rootDir.name.upper() == "FLOW") {
-	api(project(":k:klib")) {
-	  targetConfiguration = "jvmRuntimeElements"
-	}
-  } else {
-	api("matt.k:klib:+")
-  }
+  projectOrLocalMavenJVM("api", ":k:klib")
   api(projects.kj.reflect)
-  api(projects.kj.kjlib.lang)
+//  api(projects.kj.kjlib.lang)
   api(jvm(projects.k.stream))
 }
