e59e963a8aa2549c45a23e78be02e0b0020f8418
mgroth0 <mgroth49@gmail.com>
2022-06-10 00:36:21 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import matt.klib.str.upper
 modtype = LIB
 dependencies {
   projectOrLocalMavenJVM("api", ":k:klib")
-  api(projects.kj.reflect)
-  api(projects.kj.kjlib.lang)
+//  api(projects.kj.reflect)
+//  api(projects.kj.kjlib.lang)
   api(jvm(projects.k.stream))
 }
