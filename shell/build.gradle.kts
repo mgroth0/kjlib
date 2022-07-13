3937ae56b40bb2ddda1fde063d08d992efe4ab28
mgroth0 <mgroth49@gmail.com>
2022-07-13 17:51:54 -0400

    autocommit

--- shell/build.gradle.kts
+++ shell/build.gradle.kts
@@ -1,14 +1,8 @@
-
-apis(
-  ":k:klib".jvm(),
-  ":k:file".jvm()
-)
-//apis(
-//  project(":k:klib")
-//)
-
-dependencies {
-  //  implementation(projects.k.kjlib.lang)
-  api(libs.oshi)
-  implementation(projects.k.key)
+apis {
+  klib
+  file
+  libs.oshi
+}
+implementations {
+  key
 }
